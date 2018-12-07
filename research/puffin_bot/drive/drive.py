973307d58a0651d7639fd7f529038168ec7b9572
strombom <strombom@gmail.com>
2018-12-07 23:33:10 +0100

    Drive

--- research/puffin_bot/drive/drive.py
+++ research/puffin_bot/drive/drive.py
@@ -2,9 +2,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+# Adafruit motorkit requires permission to access I2C:
+#  sudo usermod -a -G i2c www-data
 from adafruit_motorkit import MotorKit
 kit = MotorKit()
 
+left_motor = kit.motor1
+right_motor = kit.motor3
 
 @app.route('/')
 def hello_world():
@@ -12,11 +16,33 @@ def hello_world():
 
 @app.route('/forward')
 def forward():
+    left_motor.throttle = 0.7
+    right_motor.throttle = 0.7
     return 'forward'
 
 @app.route('/backward')
 def backward():
+    left_motor.throttle = -0.7
+    right_motor.throttle = -0.7
     return 'backward'
+
+@app.route('/left')
+def left():
+    left_motor.throttle = -0.7
+    right_motor.throttle = 0.7
+    return 'left'
+
+@app.route('/right')
+def right():
+    left_motor.throttle = 0.7
+    right_motor.throttle = -0.7
+    return 'right'
+
+@app.route('/stop')
+def stop():
+    left_motor.throttle = None
+    right_motor.throttle = None
+    return 'stop'
 
 if __name__ == '__main__':
     app.run()
