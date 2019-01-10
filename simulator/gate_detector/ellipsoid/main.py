6f108ce29e8f7aa18cc887e354ed8af2e7db0c9e
strombom <strombom@gmail.com>
2019-01-10 01:40:58 +0100

    Line segment pairs

--- simulator/gate_detector/ellipsoid/main.py
+++ simulator/gate_detector/ellipsoid/main.py
@@ -1,29 +1,34 @@
 
 import copy
+import math
 import skimage
 import skimage.morphology
 import numpy as np
 from numba import jit, njit
 
-image = skimage.io.imread("img_in.png")
 
-#thresh = skimage.filters.threshold_minimum(image, nbins=256, max_iter=10000)
-thresh = 180
-image = image > thresh
-image = skimage.morphology.skeletonize(image)
+def read_image(filename):
+    image = skimage.io.imread(filename)
 
-# Remove edge pixels
-rr, cc = skimage.draw.line(0, 0, 0, image.shape[1]-1)
-image[rr, cc] = 0
-rr, cc = skimage.draw.line(image.shape[0]-1, 0, image.shape[0]-1, image.shape[1]-1)
-image[rr, cc] = 0
-rr, cc = skimage.draw.line(0, 0, image.shape[0]-1, 0)
-image[rr, cc] = 0
-rr, cc = skimage.draw.line(0, image.shape[1]-1, image.shape[0]-1, image.shape[1]-1)
-image[rr, cc] = 0
+    #thresh = skimage.filters.threshold_minimum(image, nbins=256, max_iter=10000)
+    thresh = 180
+    image = image > thresh
+    image = skimage.morphology.skeletonize(image)
 
-#skimage.io.imsave("img_skel.png", image*255)
-#quit()
+    # Remove edge pixels
+    rr, cc = skimage.draw.line(0, 0, 0, image.shape[1]-1)
+    image[rr, cc] = 0
+    rr, cc = skimage.draw.line(image.shape[0]-1, 0, image.shape[0]-1, image.shape[1]-1)
+    image[rr, cc] = 0
+    rr, cc = skimage.draw.line(0, 0, image.shape[0]-1, 0)
+    image[rr, cc] = 0
+    rr, cc = skimage.draw.line(0, image.shape[1]-1, image.shape[0]-1, image.shape[1]-1)
+    image[rr, cc] = 0
+
+    #skimage.io.imsave("img_skel.png", image*255)
+    #quit()
+
+    return image
 
 @jit(nopython=True)
 def extract_strip(image, ox, oy, strip):
@@ -144,7 +149,13 @@ def extract_strips(image):
 
 
 @jit(nopython=True)
-def split_strips(strips, line_segments):
+def magn(v):
+    return (v[0] * v[0] + v[1] * v[1]) ** 0.5
+
+@jit(nopython=True)
+def split_strips(strips):
+    line_segments = np.zeros((1000,2,2), dtype=np.int32)
+
     segment_count = 0
     minimum_segment_length = 10
     maximum_point_to_line_distance = 2.0
@@ -164,7 +175,7 @@ def split_strips(strips, line_segments):
 
         for idx in range(1, strip_length):
             if segment_count == line_segments.shape[0]:
-                return segment_count
+                return line_segments[0:segment_count]
 
             segment_length = idx - line_start_idx
 
@@ -175,9 +186,6 @@ def split_strips(strips, line_segments):
                     line_segments[segment_count][1] = strip[idx]
                     segment_count += 1
                 break
-
-            def magn(v):
-                return (v[0] * v[0] + v[1] * v[1]) ** 0.5
 
             # Calculate line direction
             p_dir[:] = strip[idx] - strip[line_start_idx]
@@ -197,31 +205,114 @@ def split_strips(strips, line_segments):
                 line_start_idx = idx
                 l_dir[:] = (0, 0)
 
-    return segment_count
+    return line_segments[0:segment_count]
+
+
+@jit(nopython=True)
+def min_distance(l1, l2):
+    # Find the two closest points in the line segments
+    dmin = 99999
+    min_1, min_2 = 0, 0
+    for i in range(2):
+        for j in range(2):
+            d = np.sum((l1[i] - l2[j]) ** 2) ** 0.5
+            if d < dmin:
+                dmin = d
+                min_1 = i
+                min_2 = j
+
+    # Make sure the closest points are the first points in each line segment
+    if min_1 == 1:
+        l1[:] = l1[::-1]
+    if min_2 == 1:
+        l2[:] = l2[::-1]
+
+    return dmin
+
+@jit(nopython=True)
+def intersection_angle(l1, l2):
+    v1 = l1[1] - l1[0]
+    v2 = l2[1] - l2[0]
+    dot = v1[0] * v2[0] + v1[1] * v2[1]
+    det = v1[0] * v2[1] - v1[1] * v2[0]
+    return abs(math.atan2(det, dot))
+
+@jit(nopython=True)
+def merging_degree(d, theta, l1, l2):
+    return 1 / (d*2 + 1) * math.cos(math.pi - theta) * min(l1, l2) / max(l1, l2)
+
+@jit(nopython=True)
+def line_length(line):
+    return magn(line[1] - line[0])
+
+@jit(nopython=True)
+def form_line_pairs(line_segments):
+    d_max = 5.0
+    theta_min = 145.0 / 180 * 3.14
+    line_pairs = np.zeros((1000, 2, 2, 2))
+    merging_degrees = np.zeros((1000,))
+    pair_count = 0
+
+    for i in range(line_segments.shape[0]):
+        for j in range(i + 1, line_segments.shape[0]):
+            d = min_distance(line_segments[i], line_segments[j])
+            theta = intersection_angle(line_segments[i], line_segments[j])
+            if d < d_max and theta > theta_min:
+                line_pairs[pair_count][0] = line_segments[i]
+                line_pairs[pair_count][1] = line_segments[j]
+                li, lj = line_length(line_segments[i]), line_length(line_segments[j])
+                merging_degrees[pair_count] = merging_degree(d, theta, li, lj)
+                pair_count += 1
+
+    return line_pairs[0:pair_count], merging_degrees[0:pair_count]
+
+
+def merge_line_segments(line_segments):
+
+    # Form the set LPij and sort them according to merging degree
+    line_pairs, merging_degrees = form_line_pairs(line_segments)
+    sorted_pairs = sorted(zip(line_pairs, merging_degrees), key=lambda i: i[1], reverse=True)
+    line_pairs, merging_degrees = zip(*sorted_pairs)
+
+    print(line_pairs[0:2])
+    print(merging_degrees[0:2])
+    quit()
+
+    return
+
+import pickle
+
+if False:
+    image = read_image("img_in.png")
+    strips = extract_strips(image)
+    line_segments = split_strips(strips)
+
+    with open('line_segments.dat', 'wb') as f:
+        pickle.dump(line_segments, f, pickle.HIGHEST_PROTOCOL)
+else:
+    with open('line_segments.dat', 'rb') as f:
+        line_segments = pickle.load(f)
 
 
 
-strips = extract_strips(image)
-
-line_segments = np.zeros((1000,2,2), dtype=np.int)
-line_segment_count = split_strips(strips, line_segments)
-
+arcs = merge_line_segments(line_segments)
 
 
 import random
 import seaborn as sns
-palette = sns.color_palette("hls", line_segment_count)
+palette = sns.color_palette("hls", line_segments.shape[0])
 
 im2 = np.ones((image.shape[0], image.shape[1], 3))
-for idx in range(line_segment_count):
+for idx in range(line_segments.shape[0]):
     p0, p1 = line_segments[idx]
 
     #print(p0, p1)
 
     rr, cc = skimage.draw.line(p0[1], p0[0], p1[1], p1[0])
-    im2[rr, cc] = palette[random.randrange(line_segment_count)]
+    im2[rr, cc] = palette[random.randrange(line_segments.shape[0])]
 
-skimage.io.imsave("img_line_segments.png", im2)
+
+skimage.io.imsave("img_line_segments.png", (im2 * 255).astype(np.uint8))
 quit()
 
 
